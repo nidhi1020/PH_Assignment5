19e93479316875a220ed4c3f3f81a0ea612d1802
Nidhi Biswas <nidhibiswas111@gmail.com>
2025-09-01 18:16:55 +0600

    make resposive & done heart functionalities.

--- script/index.js
+++ script/index.js
@@ -1,1 +1,21 @@
-console.log('abcd')
+
+function getElement(id){
+    const element=document.getElementById(id);
+    return element;
+}
+
+//heart
+const heartBtn=document.querySelectorAll('.heart-btn')
+const heartCnt=document.getElementById('heart-cnt');
+let totalHeart=0;
+
+for(const btn of heartBtn){
+    btn.addEventListener('click',function(){
+        totalHeart++
+        heartCnt.innerText=totalHeart;
+    }) 
+}
+
+//call btn
+
+ 
